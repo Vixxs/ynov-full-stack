1a1aa5232a289e1b250a2d3322de0ef3d2595696
Vixx <vincent.asta1@gmail.com>
2022-11-25 23:17:55 +0100

    component: checkbox

--- my-lib-ui/src/components/CheckboxComponent.tsx
+++ my-lib-ui/src/components/CheckboxComponent.tsx
@@ -3,10 +3,18 @@ import React from "react";
 type Props = React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
-> & { label: string };
+  > & { 
+    label: string,
+    idElement: string
+  };
 
 const CheckboxComponent: React.FC<Props> = (props) => {
-  return <div></div>;
+  return (
+    <div className="my-lib-ui-checkbox">
+      <input className="my-lib-ui-checkbox-input" type="checkbox" id={props.idElement} {...props}  />
+      <label className="my-lib-ui-checkbox-label" htmlFor={props.idElement}>{props.label}</label>
+    </div>
+  );
 };
 
 export default CheckboxComponent;
