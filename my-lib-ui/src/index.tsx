4ca38ba27170a2fb9c71f73b6443a2546f6d5c48
Vincent Asta <62051465+Vixxs@users.noreply.github.com>
2022-11-25 12:10:46 +0100

    fix

--- my-lib-ui/src/index.tsx
+++ my-lib-ui/src/index.tsx
@@ -1,1 +1,3 @@
 export { default as InputTextComponent } from "./components/InputTextComponent";
+export { default as SelectComponent } from "./components/SelectComponent";
+export { default as ButtonComponent } from "./components/ButtonComponent";
