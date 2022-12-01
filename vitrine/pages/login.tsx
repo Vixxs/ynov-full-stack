d11d83b6bcc1f88d317e256adf71598f07834821
Titouan <striilir@gmail.com>
2022-12-01 10:21:59 +0100

    feat(my-lib): changing attribute gestion in text input

--- vitrine/pages/login.tsx
+++ vitrine/pages/login.tsx
@@ -1,14 +1,25 @@
+import { InputTextComponent } from "my-lib-ui";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import FooterComponent from "../components/FooterComponent";
 import HeadComponent from "../components/HeadComponent";
+import HeaderComponent from "../components/HeaderComponent";
 
 const Login: NextPage = () => {
   const router = useRouter();
   return (
     <div>
       <HeadComponent />
-      <main></main>
+      <HeaderComponent />
+      <main>
+        <form className="home-form" action="">
+            <p>CONNEXION</p>
+            <div>
+              <InputTextComponent label="identifiant" type="text"/>
+              <InputTextComponent label="mot de passe" type="password"/>
+            </div>            
+          </form>
+      </main>
       <FooterComponent />
     </div>
   );
