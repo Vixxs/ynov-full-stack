f5d09f07c33e2d0b2db09e74fdf002e585f033de
Vincent ASTA <vincent.asta1@gmail.com>
2023-01-05 10:14:16 +0100

    fix: update with FullStackCourse branche

--- user/src/DataFixtures/AppFixtures.php
+++ user/src/DataFixtures/AppFixtures.php
@@ -2,31 +2,16 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Product;
-use App\Service\RickAndMortyApiService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function __construct(private readonly RickAndMortyApiService $service)
-    {
-        
-    }
-
     public function load(ObjectManager $manager): void
     {
-        $products = json_decode($this->service->loadApi());
-        if($products->results){
-            foreach($products->results as $product){
-               $productEntity = new Product();
-               $productEntity->setName($product->name); 
-               $productEntity->setPrice(rand(0, 1000));
-               $productEntity->setQuantity(rand(0,10));
-               $productEntity->setImage($product->image); 
-               $manager->persist($productEntity);
-            }
-        }
+        // $product = new Product();
+        // $manager->persist($product);
+
         $manager->flush();
     }
 }
