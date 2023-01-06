10e51b3e804e197c62d769d27e5c75f98ac09dbd
Vincent Asta <62051465+Vixxs@users.noreply.github.com>
2023-01-06 10:35:19 +0100

    fix: init proxy

--- proxy/src/app.ts
+++ proxy/src/app.ts
@@ -1,23 +1,26 @@
 import express from "express";
+import axios from "axios";
 import bodyParser from "body-parser";
 
-import checkToken from "./middlewares/checkToken";
-import userService from "./services/user";
-
-import { urlApi } from "./types";
 const app = express();
 const port = 8000;
 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(checkToken());
 
-app.get(urlApi, (_, res) => {
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/api", (_, res) => {
   res.send("Hello API");
+});
+
+app.get("/api/.user", (_, res) => {
+  axios.get("http://nginx/api/hello").then((onfulfilled) => {
+    res.send(onfulfilled.data);
+  });
 });
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
-
-userService.initUrls(app);
