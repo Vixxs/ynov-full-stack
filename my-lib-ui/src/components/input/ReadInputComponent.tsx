6f85a74d5a302ea646283975ac2a8e7eafbcf36e
Vincent ASTA <vincent.asta1@gmail.com>
2023-01-20 21:36:21 +0100

    fix: style table

--- my-lib-ui/src/components/input/ReadInputComponent.tsx
+++ my-lib-ui/src/components/input/ReadInputComponent.tsx
@@ -6,13 +6,8 @@ type Props = {
 };
 const ReadInputComponent: React.FC<Props> = ({label, value}) => {
   return (
-<<<<<<< HEAD
     <div className="my-lib-ui-read-input">
       <label>{label}</label>
-=======
-    <div className="my-lib-read-input">
-      <label >{label}</label>
->>>>>>> 18be1a2 (fix: update admin page and table/modal)
       <span>{value}</span>
     </div>
   );
